34bea766bc7d21e739b402e64f4da58ee1804562
april_al <sophi.kislik@gmail.com>
2022-08-03 19:12:22 +0300

    recursion2

---
+++ recursion/factorial.py
@@ -0,0 +1,18 @@
+def factorial(n: int):
+    res = 1
+    for i in range(1, n + 1):
+        res = res * i
+    return res
+
+v = 5
+print(factorial(v))
+
+
+def factorial_rec(n: int):
+    print(n)
+    if n == 1:
+        return 1
+    return n * factorial_rec(n - 1)
+
+
+print(factorial_rec(v))
--- recursion/utils.py
+++ recursion/utils.py
@@ -2,9 +2,10 @@ def dig_count_rec(n: int):
     n = abs(n)
     if n < 10:
         return n
-    return -1
+    return int(str(n)[0]) + dig_count_rec(int(str(n)[1:]))
 
 
-nnn = dig_count_rec("f")
+nnn = dig_count_rec(123132435445768902345768789009890786754342313)
 # nnn = "hello"
 print(nnn)
+# print('abcdefghijklmnop'[10:])
